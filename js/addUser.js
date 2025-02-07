c67c3127428f44c06fe7b78ef2b5ed2e089b9626
aanharits <farhan.harits05@gmail.com>
2025-02-08 01:41:31 +0700

    first commit

---
+++ js/addUser.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const userForm = document.getElementById('userForm');
+    const userManager = new User();
+
+    userForm.addEventListener('submit', (e) => {
+
+        e.preventDefault();
+
+        const userData = {
+            username: document.getElementById('username').value,
+        };
+
+        userManager.saveUser(userData);
+    });
+});
