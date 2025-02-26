93876b6b62afa95031dc77b73cabbc20bc7f11b3
aanharits <farhan.harits05@gmail.com>
2025-02-27 00:37:03 +0700

    update

--- js/addTask.js
+++ js/addTask.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // membuat properti tanggal yang diharapkan berformat 'yyyy-mm-dd;
-    const now = new Date();
-    const
     const taskForm = document.getElementById('taskForm');
     const taskManager = new Task();
+
+    // membuat properti tanggal yang diharapkan berformat 'yyyy-mm-dd'
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
 
     taskForm.addEventListener('submit', (e) => {
 
@@ -13,6 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const taskData = {
             taskName: document.getElementById('taskName').value,
             taskPriority: document.getElementById('taskPriority').value,
+            createdAt: `${year}-${month}-${day}`
         };
 
         const result = taskManager.addTaskUser(taskData);
