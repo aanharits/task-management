dc58072c81636f27d066a48c1072173d7a612508
aanharits <farhan.harits05@gmail.com>
2025-03-14 22:32:06 +0700

    fix code

--- js/Task.js
+++ js/Task.js
@@ -1,5 +1,4 @@
 class Task {
-
     constructor() {
         this.tasks = this.getTasks();
     }
@@ -8,20 +7,44 @@ class Task {
         return JSON.parse(localStorage.getItem('tasks')) || [];
     }
 
+    updatelocalStorage() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
+
     addTaskUser(taskData) {
         const newTaskData = {
             id: Date.now(),
             isCompleted: false,
             ...taskData,
-        }
+        };
 
-        this.tasks.push(newTaskData); 
+        this.tasks.push(newTaskData);
         localStorage.setItem('tasks', JSON.stringify(this.tasks));
 
-        return {
-            success: true,
-        };
+        return { success: true };
+    }
+
+    completeTask(taskId) {
+        console.log(taskId);
+
+        const index = this.tasks.findIndex(task => task.id == taskId);
+
+        if (index !== -1) {
+            this.tasks[index].isCompleted = true;
+            this.updatelocalStorage();
+            return { success: true };
+        }
+    }
+
+    deleteTask(taskId) {
+        console.log(taskId);
+
+        const index = this.tasks.findIndex(task => task.id == taskId);
+
+        if (index !== -1) {
+            this.tasks.splice(index, 1);
+            this.updatelocalStorage();
+            return { success: true };
+        }
     }
 }
-
-
