67433492e9809311cff06dadf06f06e167dd2b45
aanharits <farhan.harits05@gmail.com>
2025-03-16 23:18:08 +0700

    update code

--- js/signIn.js
+++ js/signIn.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const userForm = document.getElementById('userForm');
     const userManager = new User();
-
+    
     userForm.addEventListener('submit', (e) => {
 
         e.preventDefault();
